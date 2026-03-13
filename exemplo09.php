559fbad94350acf692dc2b2c98174e081989ecfe
isadorabdias-bit <isadora.b.dias@aluno.senai.br>
2026-03-13 13:53:17 -0300

    Add PHP OOP example scripts
    Add three PHP example files demonstrating basic OOP concepts:
    
    - exemplo09.php: Filme class with constructor and incrementarSaldo usage.
    - exemplo10.php: Conta class with creditar, debitar, transferir and example account interactions.
    - exemplo11.php: AreaCalc static class intended to compute circle area and access static $pi.
    
    Notes: exemplo10.php contains a likely bug in transferir (balance check appears inverted). exemplo11.php contains syntax/name errors (missing semicolon on return and a mismatched method name) that should be fixed.

---
+++ exemplo09.php
@@ -0,0 +1,24 @@
+<?php
+
+class Filme
+{
+    public $nome;
+    public $saldo;
+
+    function __construct($nome, $saldo)
+    {
+        $this->nome = $nome;
+        $this->saldo = $saldo;
+    }
+
+    function incrementarSaldo($valor)
+    {
+        $this->saldo = $this->saldo + $valor;
+    }
+}
+
+$filme = new Filme("O Poderoso Chefão", 10);
+$filme->incrementarSaldo(5);
+
+echo "Nome do Filme: $filme->nome <br>";
+echo "Saldo disponivel: $filme->saldo";
---
+++ exemplo11.php
@@ -0,0 +1,18 @@
+<?php
+
+class AreaCalc
+{
+    public static $pi = 3.141517;
+
+    static function areaCirculo($raio)
+    {
+        return self::$pi * $raio * $raio
+
+    }
+}
+
+$raioAtual = 10;
+$area = AreaCalc ::areaCircunferencia($raioAtual);
+$valorPi = AreaCalc::$pi;
+echo "Área do círculo de raio $raioAtual é $area <br>";
+echo "Valor de Pi = $valorPi";
