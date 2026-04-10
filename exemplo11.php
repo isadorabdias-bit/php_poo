42db279393d3d448816861945366ec0ab1a82f2a
isadorabdias-bit <isadora.b.dias@aluno.senai.br>
2026-04-10 12:09:54 -0300

    Fix circle area call; add Conta/Poupanca example
    Correct syntax and method usage in exemplo11.php: add the missing semicolon on the areaCirculo return and fix the call from AreaCalc::areaCircunferencia to AreaCalc::areaCirculo. Add novo arquivo exemplo12.php implementing Conta and Poupanca classes (constructor, creditar, debitar, transferir, atualizarJuros) and example usage that prints account balances.

--- exemplo11.php
+++ exemplo11.php
@@ -6,13 +6,13 @@ class AreaCalc
 
     static function areaCirculo($raio)
     {
-        return self::$pi * $raio * $raio
+        return self::$pi * $raio * $raio;
 
     }
 }
 
 $raioAtual = 10;
-$area = AreaCalc ::areaCircunferencia($raioAtual);
+$area = AreaCalc ::areaCirculo($raioAtual);
 $valorPi = AreaCalc::$pi;
 echo "Área do círculo de raio $raioAtual é $area <br>";
 echo "Valor de Pi = $valorPi";
